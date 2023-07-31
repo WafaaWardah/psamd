2ab2d8bdcbfc11cb15857dde2401fdfa7d77e17f
Wafaa <wafaawardah89@gmail.com>
2023-08-01 01:29:22 +0200

    Commenced dev

--- lib.py
+++ lib.py
@@ -3,7 +3,9 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import torch
 import torchaudio
+import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
@@ -36,13 +38,44 @@ class DimDataset(Dataset):
         return len(self.df)
     
 
+class SQAdataset(Dataset):
+
+    def __init__(self, df):
+        super().__init__()
+
+        self.df = df
+
+    def __getitem__(self, index):
+        return self.df(index)
+    
+
 #--------------------------- model class ----------------------------------------
+class ModelDim(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.bundle = torchaudio.pipelines.WAV2VEC2_XLSR53
+        self.feature_extractor = self.bundle.get_model()
+
+
+    def forward(self, x_padded, x_lens):
+
+        for n, (p, l) in enumerate(zip(x_padded, x_lens)):
+            print(n, len(p), l)
+
+        with torch.inference_mode():
+            features, _ = self.feature_extractor.extract_features(x_padded, num_layers=7)
+        y_pred = torch.stack(features, dim=0)
+        y_pred = torch.squeeze(y_pred)
+
+        print(f'length of y_pred: {len(y_pred)}')
+        return y_pred
 
 
 
 #--------------------------- functions ----------------------------------------
 
-def load_data(data_dir, csv_file, db_dict, sample_rate, max_length):
+def get_set(data_dir, csv_file, db_dict, sample_rate, max_length):
     # load dataframes
     dfile = pd.read_csv(os.path.join(data_dir, csv_file))
     if not set(db_dict).issubset(dfile.db.unique().tolist()):
@@ -52,10 +85,29 @@ def load_data(data_dir, csv_file, db_dict, sample_rate, max_length):
     # instantiate Pytorch data class 
     return df, DimDataset(df, data_dir, sample_rate, max_length)
 
+
+def load_data(dir, csv, tr_db, val_db, sr, max_len, feature_extractor):
+    print(f'\nLoading training set...')
+    dfile = pd.read_csv(os.path.join(dir, csv))
+    if not set(tr_db + val_db).issubset(dfile.db.unique().tolist()):
+        missing_datasets = set(tr_db + val_db).difference(dfile.db.unique().tolist())
+        raise ValueError('Not all dbs found in csv:', missing_datasets)
+    tr_df = dfile[dfile.db.isin(tr_db)].reset_index()
+    val_df = dfile[dfile.db.isin(val_db)].reset_index()
+
+    print(f'\n\nLoading datasets - created dfs.\n\nTrain df is \n{tr_df}\Val df is \n{val_df}')
+    
+    tr_set = SQAdataset(tr_df) # dataset with feature extraction performed in getitem()
+    val_set = SQAdataset(val_df)
+
+    return  tr_df, tr_set, val_df, val_set
+
+
 def pad_collate(batch):
     (xx, yy, idx) = zip(*batch)
     xx = [x.squeeze(0) for x in xx]
-    #x_lens = [len(x) for x in xx]
+    x_lens = [len(x) for x in xx]
+
     #y_lens = [len(y) for y in yy]
     xx_pad = pad_sequence(xx, batch_first=True, padding_value=0)
-    return xx_pad, yy, idx
+    return xx_pad, x_lens, yy, idx
--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import sys
 import torchaudio
 import lib
 from torch.utils.data import DataLoader
+from torch.optim import Adam
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--yaml', required=True, type=str, help='YAML file with config')
@@ -26,25 +27,57 @@ if __name__ == "__main__":
     print(f'\n\n-----------------------------\n\tModel Training\n-----------------------------')
 
     # load data
-    tr_df, trainset = lib.load_data(args['data_dir'], args['csv_file'], args['csv_db_train'], 
-                             args['sample_rate'], args['max_length'])
-    val_df, valset = lib.load_data(args['data_dir'], args['csv_file'], args['csv_db_val'], 
-                           args['sample_rate'], args['max_length'])
+
+    bundle = torchaudio.pipelines.WAV2VEC2_XLSR53
+    feature_extractor = bundle.get_model()
+    tr_df, trainset, val_df, valset = lib.load_data(args['data_dir'], args['csv_file'], 
+                                    args['csv_db_train'], args['csv_db_train'], 
+                                    args['sample_rate'], args['max_length'], feature_extractor)
+    
+    
 
     trainloader = DataLoader(trainset, batch_size=args['batch_size'], shuffle=False, collate_fn=lib.pad_collate)
     valloader = DataLoader(valset, batch_size=args['batch_size'], shuffle=False, collate_fn=lib.pad_collate)
 
     # load model
-    
+    model = lib.ModelDim()
+
+    print('\n\nInvestigating thte model feature extractor:\n')
+
+    state = model.feature_extractor.state_dict()
+
+    #print(f'state is {state}')
+
+    for i, x in enumerate(state):
+        print(i, x)
+
+    sys.exit()
+
+
+    criterion = torch.nn.MSELoss(reduction='none')
+    optimizer = Adam(model.parameters(), lr=0.00001)
 
     # train loop for each loop
 
     for epoch in range(args['max_epochs']):
 
         # train loop
-        for xx_pad, yy, idx in trainloader:
-            print(f'indices {idx} batch_x shape is {xx_pad.shape}, batch_y shape is {len(yy)}')
+        for b, (xx_pad, x_lens, yy, idx) in enumerate(trainloader):
+            print(f'\n\nbatch {b} indices {idx} batch_x shape is {xx_pad.shape}, batch_y shape is {len(yy)}\n')
+            outputs = model(xx_pad, x_lens)
 
+            outputs2 = model(xx_pad, x_lens)
+
+            print(len(outputs))
+            print(len(outputs2))
+
+
+            for i, j in zip(outputs, outputs2):
+
+                print(f'i shape {i.shape}, j shape {j.shape}')
+
+        
+        sys.exit()
 
         # validation loop
 
